dc38f3db139e36b23f1d46714c7cc276d5dae666
EvgGrinev <grinev96@gmail.com>
2022-12-05 09:31:33 +0300

    first

---
+++ index-test.js
@@ -0,0 +1,34 @@
+const assert = require('assert');
+const Rooster = require('../index')
+
+describe('Rooster', () => {
+  describe('.announceDawn', () => {
+    it('returns a rooster call', () => {
+      const expected = 'cock-a-doodle-doo!';
+      const actual = Rooster.announceDawn();
+      assert.equal(actual, expected);
+    });
+  });
+  describe('.timeAtDawn', () => {
+    it('returns its argument as a string', () => {
+      const inputNum = 12
+      const expected = '12';
+      const actual = Rooster.timeAtDawn(inputNum);
+      assert.strictEqual(actual, expected);
+    });
+    it('throws an error if passed a number less than 0', () => {
+      const inputNum = -1
+      const expected = RangeError;
+      assert.throws( () => {
+          Rooster.timeAtDawn(inputNum)},
+          expected);
+      });
+    it('throws an error if passed a number less than 23', () => {
+      const inputNum = 24
+      const expected = RangeError;
+      assert.throws( () => {
+          Rooster.timeAtDawn(inputNum)},
+          expected);
+      });
+  });   
+})
